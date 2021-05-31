f323f733b7eee56d28f5505087946fcabe466fe8
João Vitor Pontes <joaovitorpontesguzo@gmail.com>
2021-05-30 21:09:55 -0300

    terminando os exercicios restantes

--- quinzena1/lista-logica/out/exercicios.js
+++ quinzena1/lista-logica/out/exercicios.js
@@ -22,7 +22,7 @@ function imprimeMensagem() {
 
 // Exercício 1
 function calculaAreaRetangulo() {
-  // escreva seu código aqui
+  
   const altura = prompt('Digite a altura do seu retangulo')
   const largura = prompt('Digite a largura de seu retangulo')
 
@@ -32,7 +32,7 @@ function calculaAreaRetangulo() {
 
 // Exercício 2
 function imprimeIdade() {
-  // escreva seu código aqui
+
   const anoAtual = prompt('Digite o ano em que estamos')
   const anoNasc = prompt('Agora digite o ano em que você nasceu')
 
@@ -42,7 +42,7 @@ function imprimeIdade() {
 
 // Exercício 3
 function calculaIMC() {
-  // escreva seu código aqui
+
   const peso = prompt('Digite o seu peso em kg')
   const altura = prompt('Agora digite sua altura em M(pode ser usado uma casa decimal Ex:1.78')
 
@@ -52,45 +52,91 @@ function calculaIMC() {
 
 // Exercício 4
 function imprimeInformacoesUsuario() {
-  // escreva seu código aqui
+
+  const nome = prompt('Digite o seu nome')    
+  const idade = prompt('Digite sua idade')
+  const email = prompt("Digite seu e-mail")
+  
+  
+  console.log ("Meu nome é " +nome+ ", tenho " + idade + " anos, e o meu email é " +email+ ".")
 }
+
 
 // Exercício 5
 function imprimeTresCoresFavoritas() {
-  // escreva seu código aqui
+
+  const corUm = prompt('Digite a primeira cor favorita')    
+  const corDois = prompt('Digite a segunda cor favorita')
+  const corTres= prompt("Digite a terceira cor favorita")
+
+  console.log ([""+corUm+"",""+corDois+"",""+corTres+""])
+  
 }
 
 // Exercício 6
 function retornaStringEmMaiuscula() {
-  // escreva seu código aqui
+ 
+  let frase = prompt('Digite a frase')
+
+  console.log(frase.toUpperCase()); 
 }
 
 // Exercício 7
 function calculaIngressosEspetaculo() {
-  // escreva seu código aqui
+  const custo = prompt("Digite o custo do espetaculo")
+  const valorIngresso = prompt("Agora digite o valor de cada ingresso")
+
+  const qtdIngressos = (custo / valorIngresso)
+
+  console.log(qtdIngressos)
+
 }
 
 // Exercício 8
 function checaStringsMesmoTamanho() {
-  // escreva seu código aqui
+  const string1 = prompt("Digite aqui o que deseja checar")
+  const string2 = prompt("Digite aqui para compararmos")
+
+  console.log(string1.length === string2.length)
 }
 
 // Exercício 9
 function checaIgualdadeDesconsiderandoCase() {
-  // escreva seu código aqui
+  const string1 = prompt("Digite aqui o que deseja checar")
+  const string2 = prompt("Digite aqui para compararmos")
+
+  console.log(string1.toUpperCase() === string2.toUpperCase())
 }
 
-// Exercício 10
+//Exercicio 10
 function checaRenovacaoRG() {
-  // escreva seu código aqui
+  const anoAtual= prompt('Digite o ano atual')    
+  const anoNasc = prompt('Digite o ano de nascimento')
+  const anoEmissao= prompt("Digite ao ano de emissão de seu RG")
+  
+  const idade = (anoAtual - anoNasc)
+  const emissao = (anoAtual - anoEmissao)
+
+  console.log((idade <= 20 && emissao >= 5) || (idade > 20 && idade <= 50 && emissao >= 10) ||
+   (idade > 50 && emissao >= 15))
+  
 }
 
 // Exercício 11
 function checaAnoBissexto() {
-  // escreva seu código aqui
+
+  const anoAtual = prompt("Digite o ano que estamos")
+  const resultado = (anoAtual % 400 == 0) || (anoAtual % 4 == 0 && anoAtual % 100 != 0)
+
+  console.log(resultado)
+  
 }
 
 // Exercício 12
 function checaValidadeInscricaoLabenu() {
-  // escreva seu código aqui
+  const idade = prompt("É maior de 18 anos?")
+  const escolaridade = prompt("Possui ensino medio completo")
+  const disponibilidade = prompt("Possui disponibilidade exclusiva pro curso")
+
+  console.log(idade =="sim" && escolaridade =="sim" && disponibilidade =="sim");
 }
